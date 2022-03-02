fb856d5d6c49c76b9b85197d053fe975b33cd0f6
Luan Menezes <luanmenezes99@gmail.com>
2022-03-01 23:36:25 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a1f4b09ce93894d96509204b5d3b1dc4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3bba09c-36a8-44c6-b169-440ff9b392b6'
+};
